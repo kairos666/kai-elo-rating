ab07cf760ab81337201d54ca990da7525c000723
David Maggi <musashi.miyamoto81@gmail.com>
2022-02-27 17:22:46 +0100

    refactor assertion comparing floating numbers

--- test/elo-engine.test.ts
+++ test/elo-engine.test.ts
@@ -1,21 +1,16 @@
 import { expectedScore, scoreDifferential, monoMatchCalculator, multiMatchCalculator } from '../src/elo-engine';
 
-// utils
-function twoDecimalsRounding(value:number):number {
-    return Math.round((value + Number.EPSILON) * 100) / 100;
-}
-
 describe("elo-engine", () => {
     describe("expectedScore", () => {
         it(`should return the correct expected match result probability`, () => {
-            expect(expectedScore({ playerRank: 1613, opponentRank: 1613 })).toBe(0.5);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 1613, opponentRank: 1609 }))).toBe(0.51);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 1613, opponentRank: 1477 }))).toBe(0.69);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 1613, opponentRank: 1388 }))).toBe(0.79);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 1613, opponentRank: 1586 }))).toBe(0.54);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 1613, opponentRank: 1720 }))).toBe(0.35);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 2600, opponentRank: 2300 }))).toBe(0.85);
-            expect(twoDecimalsRounding(expectedScore({ playerRank: 2300, opponentRank: 2600 }))).toBe(0.15);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1613 })).toBeCloseTo(0.5);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1609 })).toBeCloseTo(0.51);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1477 })).toBeCloseTo(0.69);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1388 })).toBeCloseTo(0.79);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1586 })).toBeCloseTo(0.54);
+            expect(expectedScore({ playerRank: 1613, opponentRank: 1720 })).toBeCloseTo(0.35);
+            expect(expectedScore({ playerRank: 2600, opponentRank: 2300 })).toBeCloseTo(0.85);
+            expect(expectedScore({ playerRank: 2300, opponentRank: 2600 })).toBeCloseTo(0.15);
         });
     });
 
@@ -25,19 +20,19 @@ describe("elo-engine", () => {
                 actualScore: 0, // 1 loss
                 expectedScore: 0.849, // expected score with 2600 pts against 2300 opponent
                 kFactor: 16
-            })).toBe(-13.584);
+            })).toBeCloseTo(-13.584, 3);
             expect(scoreDifferential({
                 actualScore: 1, // 1 win
                 expectedScore: 0.151, // expected score with 2300 pts against 2600 opponent
                 kFactor: 16
-            })).toBe(13.584);
+            })).toBeCloseTo(13.584, 3);
         });
         it(`should return the correct ELO score differential based on several matches outcome, k factor and expected result against opponents`, () => {
             expect(scoreDifferential({
                 actualScore: 2.5, // 0 + 0.5 + 1 + 1 + 0 (2 wins, 2 losses, 1 draw)
                 expectedScore: 2.88, // 0.51 + 0.69 + 0.79 + 0.54 + 0.35 (expected scores with 1613 pts against opponents with: 1609, 1477, 1388, 1586, 1720)
                 kFactor: 32
-            })).toBe(-12.159999999999997);
+            })).toBeCloseTo(-12.159999999999997, 10);
         });
     });
     
