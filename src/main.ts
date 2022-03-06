a9753d3c485527de7795e09e1be8536f1aebc033
David Maggi <musashi.miyamoto81@gmail.com>
2022-03-06 21:53:18 +0100

    main kfactor rules export

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+import { fixedKFactorRuleBuilder, adaptToPlayerRankKFactorRuleBuilder } from "./elo-kfactor-rules";
 import { EloRankingBoard_InMemory } from "./elo-ranks-board";
 
 export const EloRankingBoardInMemory = EloRankingBoard_InMemory;
+
+// BUILT-IN K factor rule makers
+export const fixedKFactorRuleMaker = fixedKFactorRuleBuilder;
+export const adaptToPlayerRankKFactorRuleMaker = adaptToPlayerRankKFactorRuleBuilder;
